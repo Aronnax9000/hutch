9ea73a2af6af905aabe7a79210a3bf148c4215ef
Aronnax9000 <aronnax9000@gmail.com>
2023-01-13 14:13:49 -0500

    Authentication module (Devise). Barn Crud done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
-Rails.application.routes.draw do
-  get 'welcome/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "hutches#index"
-end
 
 Rails.application.routes.draw do
+  devise_for :users
   root "welcome#index"
   get 'welcome/index'
   resources :barns
